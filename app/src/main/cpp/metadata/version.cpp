47dc46d8e8f126c152f6ef55738d52c777a12c5f
Kostas Tzevanidis <ktzevanid@gmail.com>
2021-01-28 16:07:48 +0200

    APK Release v0.3.0-7.

--- app/src/main/cpp/metadata/version.cpp
+++ app/src/main/cpp/metadata/version.cpp
@@ -1,5 +1,5 @@
 extern const int PROJECT_VERSION_MAJOR=0;
 extern const int PROJECT_VERSION_MINOR=3;
 extern const int PROJECT_VERSION_PATCH=0;
-extern const int PROJECT_VERSION_BUILD_NUMBER=6;
-extern const int PROJECT_BUILD_NUMBER=1891;
+extern const int PROJECT_VERSION_BUILD_NUMBER=7;
+extern const int PROJECT_BUILD_NUMBER=1892;
